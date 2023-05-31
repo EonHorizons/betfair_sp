f7f722e9dc899d42bb922a73fde99ff9e7e0e47c
EonHorizons <98542310+EonHorizons@users.noreply.github.com>
2023-05-31 22:43:40 +0000

    delete duplicates run

--- delete_duplicates.py
+++ delete_duplicates.py
@@ -1,6 +1,10 @@
 import os
 
 
+# Github repo folder destination
+# download_folder = 'data/'
+download_folder = '/workspaces/betfair_sp/data'
+
 # delete duplicate files in repository folder
 def delete_files_with_parentheses(directory):
     """ Deletes all files in the given directory whose name contains parentheses. """
@@ -21,4 +25,5 @@ def delete_files_not_ending_with_csv(directory):
 
 
 if __name__ == "__main__":
-    pass
+    delete_files_with_parentheses(download_folder)
+    delete_files_not_ending_with_csv(download_folder)
