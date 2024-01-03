e2a8010f5b1e9206943e558b03ca9f70baf41726
EonHorizons <98542310+EonHorizons@users.noreply.github.com>
2024-01-03 14:48:59 +1000

    code change

--- get_bsp_files.py
+++ get_bsp_files.py
@@ -40,8 +40,32 @@ def create_webpage_data_json():
     # URL of the Betfair Starting Price (BSP) webpage
     url = 'https://promo.betfair.com/betfairsp/prices'
 
+        # Headers with User-Agent
+    headers = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'max-age=0',
+    'dnt': '1',
+    'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',    
+    'Origin' : 'https://promo.betfair.com',
+    'Referer' : 'https://promo.betfair.com/',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'
+}
+    # headers = {
+    #     'Origin' : 'https://promo.betfair.com',
+    #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    #     'Referer' : 'https://promo.betfair.com/'
+    #     }
+
     # GET request to webpage to retrieve HTML content
-    response = requests.get(url)
+    response = requests.get(url, headers=headers)
 
     if response.status_code != 200:
         raise Exception(f"Failed to fetch webpage: Status code {response.status_code}")
@@ -61,5 +85,5 @@ def create_webpage_data_json():
 
 if __name__ == "__main__":
     # Run the script
-    # create_webpage_data_json()
-    check_access()
+    create_webpage_data_json()
+    # check_access()
